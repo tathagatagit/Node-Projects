171707a2a42b86b9f32aadafb36dcaabc2040ce1
tathagatagit <b.tathagata@gmail.com>
2025-11-20 00:12:38 +0530

    DB connect string revoved

--- JWT_Auth/auth/auth.model.js
+++ JWT_Auth/auth/auth.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect("mongodb://localhost:27017/admin").then(() => {
+mongoose.connect("").then(() => {
     console.log("DB connected");
 }).catch((e) => {
     console.log(`Db not connected ${e.message}`);
